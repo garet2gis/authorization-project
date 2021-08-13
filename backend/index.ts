79fe51e3909c962b28883a52ce5064155f481d98
mukovskii.daniil <br-avm32@yandex.ru>
2021-08-13 17:57:12 +0300

    configure backend of project

---
+++ backend/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+dotenv.config({ path: `${__dirname}/.env` });
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
